7708227ac8914f5080007e88859484d76f3c7538
Farzad Farazmand <farzad.farazmand@gmail.com>
2018-01-17 17:50:08 +0330

    -add slider in demo app

--- demo/src/main/java/ir/apend/slidersample/MainActivity.java
+++ demo/src/main/java/ir/apend/slidersample/MainActivity.java
@@ -2,6 +2,15 @@ package ir.apend.slidersample;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.AdapterView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import ir.apend.slider.model.Slide;
+import ir.apend.slider.ui.Slider;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +18,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        Slider slider = findViewById(R.id.slider);
+
+        List<Slide> slideList = new ArrayList<>();
+        slideList.add(new Slide(0,"http://cssslider.com/sliders/demo-20/data1/images/picjumbo.com_img_4635.jpg" , getResources().getDimensionPixelSize(R.dimen.slider_image_corner)));
+        slideList.add(new Slide(1,"http://cssslider.com/sliders/demo-12/data1/images/picjumbo.com_hnck1995.jpg" , getResources().getDimensionPixelSize(R.dimen.slider_image_corner)));
+        slideList.add(new Slide(2,"http://cssslider.com/sliders/demo-19/data1/images/picjumbo.com_hnck1588.jpg" , getResources().getDimensionPixelSize(R.dimen.slider_image_corner)));
+        slideList.add(new Slide(3,"http://wowslider.com/sliders/demo-18/data1/images/shanghai.jpg" , getResources().getDimensionPixelSize(R.dimen.slider_image_corner)));
+
+        slider.setItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                //do what you want
+            }
+        });
+        slider.addSlides(slideList);
     }
 }
